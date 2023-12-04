4895b739d4f1cc18d68f3523ab083c65b35c4cc8
MaKollm <150438536+MaKollm@users.noreply.github.com>
2023-12-04 22:54:38 +0100

    Update test_output.py

--- test/test_output.py
+++ test/test_output.py
@@ -1,9 +1,7 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'src'))
 
-#print(sys.path)
-
-from optimization import test
+#from optimization import test
 
 def test_run_output():
     result = 1
